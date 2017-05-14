72973167f7f745c578e5cbc6430906e5b018e156
qqc <qqc@admindeiMac.local>
2017-05-14 17:07:38 +0800

    完善

---
+++ QqcFontDef.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+
+  s.license      = "MIT"
+  s.author       = { "qqc" => "20599378@qq.com" }
+  s.platform     = :ios, "8.0"
+  s.requires_arc  = true
+
+  s.name         = "QqcFontDef"
+  s.version      = "1.0.0"
+  s.summary      = "QqcFontDef"
+  s.homepage     = "https://github.com/xukiki/QqcFontDef"
+  s.source       = { :git => "https://github.com/xukiki/QqcFontDef.git", :tag => "#{s.version}" }
+  
+  s.source_files  = ["QqcFontDef/*.{h,m}"]
+
+end
---
+++ QqcFontDef/QqcFontDef.h
@@ -0,0 +1,59 @@
+//
+//  QqcFontDef.h
+//  QqcBaseFramework
+//
+//  Created by qqc on 13-6-20.
+//  Copyright (c) 2013年 Qqc. All rights reserved.
+//
+
+#ifndef QqcBaseFramework_QqcFontDef_h
+#define QqcBaseFramework_QqcFontDef_h
+
+
+//字体定义
+#define font_40_qqc [UIFont systemFontOfSize:40.0]
+#define font_24_qqc [UIFont systemFontOfSize:24.0]
+#define font_23_qqc [UIFont systemFontOfSize:23.0]
+#define font_22_qqc [UIFont systemFontOfSize:22.0]
+#define font_21_qqc [UIFont systemFontOfSize:21.0]
+#define font_20_qqc [UIFont systemFontOfSize:20.0]
+#define font_19_qqc [UIFont systemFontOfSize:19.0]
+#define font_18_qqc [UIFont systemFontOfSize:18.0]
+#define font_17_qqc [UIFont systemFontOfSize:17.0]
+#define font_16_qqc [UIFont systemFontOfSize:16.0]
+#define font_15_qqc [UIFont systemFontOfSize:15.0]
+#define font_14_qqc [UIFont systemFontOfSize:14.0]
+#define font_13_qqc [UIFont systemFontOfSize:13.0]
+#define font_12_qqc [UIFont systemFontOfSize:12.0]
+#define font_11_qqc [UIFont systemFontOfSize:11.0]
+#define font_10_qqc [UIFont systemFontOfSize:10.0]
+#define font_9_qqc  [UIFont systemFontOfSize:9.0]
+#define font_8_qqc  [UIFont systemFontOfSize:8.0]
+
+#define font_blod_30_qqc      [UIFont boldSystemFontOfSize:30.0]
+#define font_blod_20_qqc      [UIFont boldSystemFontOfSize:20.0]
+#define font_blod_14_qqc      [UIFont boldSystemFontOfSize:14.0]
+#define font_blod_12_qqc      [UIFont boldSystemFontOfSize:12.0]
+
+
+//...字体定义 end...
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
